cda405182ff00f573c33514e92fd7e2377de8c93
Karim <Bentahar.dev@gmail.com>
2023-03-11 00:17:18 +0100

    Make an automatic cookie login system

--- logout.php
+++ logout.php
@@ -3,5 +3,7 @@
     session_unset();    // Désactiver la session
     session_destroy();  // Détruir la session
 
+    setcookie("auth", "", time() - 1);
+
     header("location: index.php");
     exit();
